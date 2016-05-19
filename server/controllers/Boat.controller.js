602123cf15652e33275c22e61e4d9ab84954bab3
Daniel Wong <danielwong2268@gmail.com>
2016-05-19 00:49:00 -0700

    Refactor timeslots out of Boat.controller into its own module

--- server/controllers/Boat.controller.js
+++ server/controllers/Boat.controller.js
@@ -1,33 +1,13 @@
 // contains the api endpoints for the Boat Tour
 
-// store the data:
-// store id, start time, , boats, availability, customer_count 
-
-// this stores ID as key, for lookup by ID
-var timeSlotsById = {};
-// this stores timeslots with the day as key, in YYYY-MM-DD format
-var timeSlotsByDate = {};
-
-
-var Timeslot = ( () => {
-  var count = 1;
-  return function(startTime, duration) {
-    this.start_time = startTime;
-    this.id = 'timeslot' + count;
-    this.boats = [];
-    this.availability = 0;
-    this.customer_count = 0;
-    count++;
-  }
-})();
-
+var Timeslots = require('../timeslots.js');
 
 module.exports = {
   createTimeslots: (req, res) => {
     var start = req.body['timeslot[start_time]'];
     var duration = req.body['timeslot[duration]'];
 
-    var timeSlot = new Timeslot(start, duration);
+    var timeSlot = new Timeslots.create(start, duration);
 
     // convert start time to date format
     var date = new Date(start * 1000),
@@ -36,17 +16,20 @@ module.exports = {
         day = date.getUTCDate();
     var formattedDate = year + '-' + month + '-' + day;
 
-    timeSlotsById[timeSlot.id] = timeSlot;
-    timeSlotsByDate[formattedDate] = timeSlot;
+    Timeslots.byId[timeSlot.id] = timeSlot;
+    Timeslots.byDate[formattedDate] = timeSlot;
 
     res.send(timeSlot);
   },
 
   getTimeslots: (req, res) => {
-
+    var date = req.params;
+    console.log('date', date);
   },
 
   createBoat: (req, res) => {
+    var capacity = req.body['boat[capacity]'];
+    var name = req.body['boat[name]'];
 
   },
 
---
+++ server/timeslots.js
@@ -0,0 +1,16 @@
+module.exports = {
+  byId: {},
+  byDate: {},
+  create: ( () => {
+    var count = 1;
+    return function(startTime, duration) {
+      this.start_time = startTime;
+      this.id = 'timeslot' + count;
+      this.boats = [];
+      this.availability = 0;
+      this.customer_count = 0;
+      count++;
+    }
+  })()
+
+}
